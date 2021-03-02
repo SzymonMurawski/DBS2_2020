c97356734eb00e5dacc2b4bdf93e129ffb578706
Szymon Murawski <szymon.murawski@gmail.com>
2021-03-02 14:32:27 +0100

    Different ways of refering to columns in results

--- NpgsqlIntroduction/NpgsqlIntroduction/Program.cs
+++ NpgsqlIntroduction/NpgsqlIntroduction/Program.cs
@@ -10,12 +10,12 @@ namespace NpgsqlIntroduction
             NpgsqlConnection conn = new NpgsqlConnection("Server=127.0.0.1;User Id=postgres;Password=pwd;Database=rental;");
             conn.Open();
 
-            NpgsqlCommand cmd = new NpgsqlCommand("SELECT title FROM movies", conn);
-
+            NpgsqlCommand cmd = new NpgsqlCommand("SELECT title, price, movie_id  FROM movies", conn);
             NpgsqlDataReader dataReader = cmd.ExecuteReader();
             while (dataReader.Read())
             {
-                Console.WriteLine($"title: {dataReader[0]}");
+                //Console.WriteLine($"Movie {dataReader[2]} has title \"{dataReader[0]}\" and price: {dataReader[1]}");
+                Console.WriteLine($"Movie {dataReader["movie_id"]} has title \"{dataReader["title"]}\" and price: {dataReader["price"]}");
             }
 
             conn.Close();
