a45ff069a90e94956e654a148e1ac551652f053c
Szymon Murawski <szymon.murawski@gmail.com>
2021-03-02 14:02:59 +0100

    Init

---
+++ NpgsqlIntroduction/NpgsqlIntroduction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NpgsqlIntroduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
