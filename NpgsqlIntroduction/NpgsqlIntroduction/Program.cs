257b3fdecd62197da00e82810435a3e759b4d937
Szymon Murawski <szymon.murawski@gmail.com>
2021-03-09 14:12:26 +0100

    using block

--- NpgsqlIntroduction/NpgsqlIntroduction/Program.cs
+++ NpgsqlIntroduction/NpgsqlIntroduction/Program.cs
@@ -7,40 +7,45 @@ namespace NpgsqlIntroduction
     {
         static void Main(string[] args)
         {
+            string connection_string = "Server=127.0.0.1;User Id=postgres;Password=pwd;Database=rental;";
             Console.Write("Display movies produced in year:");
             string year = Console.ReadLine();
-            NpgsqlConnection conn = new NpgsqlConnection("Server=127.0.0.1;User Id=postgres;Password=pwd;Database=rental;");
-            conn.Open();
 
-            NpgsqlCommand cmd = new NpgsqlCommand("SELECT title, price, movie_id FROM movies WHERE year = @year", conn);
-            cmd.Parameters.AddWithValue("@year", int.Parse(year));
-            NpgsqlDataReader dataReader = cmd.ExecuteReader();
-            while (dataReader.Read())
+            using (NpgsqlConnection conn = new NpgsqlConnection(connection_string))
             {
-                //Console.WriteLine($"Movie {dataReader[2]} has title \"{dataReader[0]}\" and price: {dataReader[1]}");
-                Console.WriteLine($"Movie {dataReader["movie_id"]} has title \"{dataReader["title"]}\" and price: {dataReader["price"]}");
+                using (NpgsqlCommand cmd = new NpgsqlCommand("SELECT title, price, movie_id FROM movies WHERE year = @year", conn))
+                {
+                    cmd.Parameters.AddWithValue("@year", int.Parse(year));
+                    NpgsqlDataReader dataReader = cmd.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        //Console.WriteLine($"Movie {dataReader[2]} has title \"{dataReader[0]}\" and price: {dataReader[1]}");
+                        Console.WriteLine($"Movie {dataReader["movie_id"]} has title \"{dataReader["title"]}\" and price: {dataReader["price"]}");
+                    }
+                }
             }
-            conn.Close();
 
-            conn.Open();
-            cmd = new NpgsqlCommand("SELECT MAX(movie_id)+1 FROM movies", conn);
-            int new_id = (int)cmd.ExecuteScalar();
-            conn.Close();
+            int new_id;
+            using (NpgsqlConnection conn = new NpgsqlConnection(connection_string))
+            {
+                var cmd = new NpgsqlCommand("SELECT MAX(movie_id)+1 FROM movies", conn);
+                new_id = (int)cmd.ExecuteScalar();
+            }
 
-            conn.Open();
-            Console.WriteLine("Title of new movie: ");
-            string title = Console.ReadLine();
-            Console.WriteLine("Year of new movie: ");
-            int year2 = int.Parse(Console.ReadLine());
+            using (NpgsqlConnection conn = new NpgsqlConnection(connection_string))
+            { 
+                Console.WriteLine("Title of new movie: ");
+                string title = Console.ReadLine();
+                Console.WriteLine("Year of new movie: ");
+                int year2 = int.Parse(Console.ReadLine());
 
-            var cmd2 = new NpgsqlCommand("INSERT INTO movies(movie_id, title, year) VALUES (@id, @title, @year)", conn);
-            cmd2.Parameters.AddWithValue("@id", new_id);
-            cmd2.Parameters.AddWithValue("@title", title);
-            cmd2.Parameters.AddWithValue("@year", year2);
+                var cmd2 = new NpgsqlCommand("INSERT INTO movies(movie_id, title, year) VALUES (@id, @title, @year)", conn);
+                cmd2.Parameters.AddWithValue("@id", new_id);
+                cmd2.Parameters.AddWithValue("@title", title);
+                cmd2.Parameters.AddWithValue("@year", year2);
 
-            cmd2.ExecuteNonQuery();
-
-            conn.Close();
+                cmd2.ExecuteNonQuery();
+            }
         }
     }
 }
