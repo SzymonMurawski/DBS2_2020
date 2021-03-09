8c244dd556c42eb266f37172ff061d6198738da2
Szymon Murawski <szymon.murawski@gmail.com>
2021-03-09 14:08:28 +0100

    Inserting new rows into table

--- NpgsqlIntroduction/NpgsqlIntroduction/Program.cs
+++ NpgsqlIntroduction/NpgsqlIntroduction/Program.cs
@@ -20,8 +20,25 @@ namespace NpgsqlIntroduction
                 //Console.WriteLine($"Movie {dataReader[2]} has title \"{dataReader[0]}\" and price: {dataReader[1]}");
                 Console.WriteLine($"Movie {dataReader["movie_id"]} has title \"{dataReader["title"]}\" and price: {dataReader["price"]}");
             }
+            conn.Close();
 
+            conn.Open();
+            cmd = new NpgsqlCommand("SELECT MAX(movie_id)+1 FROM movies", conn);
+            int new_id = (int)cmd.ExecuteScalar();
+            conn.Close();
 
+            conn.Open();
+            Console.WriteLine("Title of new movie: ");
+            string title = Console.ReadLine();
+            Console.WriteLine("Year of new movie: ");
+            int year2 = int.Parse(Console.ReadLine());
+
+            var cmd2 = new NpgsqlCommand("INSERT INTO movies(movie_id, title, year) VALUES (@id, @title, @year)", conn);
+            cmd2.Parameters.AddWithValue("@id", new_id);
+            cmd2.Parameters.AddWithValue("@title", title);
+            cmd2.Parameters.AddWithValue("@year", year2);
+
+            cmd2.ExecuteNonQuery();
 
             conn.Close();
         }
