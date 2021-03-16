27d525dc82c9f2ef830f0a7caf10fe7aa59feaee
Szymon Murawski <szymon.murawski@gmail.com>
2021-03-16 14:37:07 +0100

    Saving 1/2

--- ActiveRecord/ActiveRecord/MovieRecord.cs
+++ ActiveRecord/ActiveRecord/MovieRecord.cs
@@ -7,6 +7,7 @@ namespace ActiveRecord
 {
     class MovieRecord
     {
+        private string connection_string = "Server=127.0.0.1;User Id=postgres;Password=pwd;Database=rental;";
         public int Id { get; set; }
         public string Title { get; set; }
         public int Year { get; set; }
@@ -18,15 +19,46 @@ namespace ActiveRecord
             Year = year;
             Price = price;
         }
-
-        public static MovieRecord GetById(int id)
+        public MovieRecord(int id)
         {
-            throw new NotImplementedException();
+            using (var conn = new NpgsqlConnection(connection_string))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand("SELECT * FROM movies WHERE movie_id = @id", conn))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    NpgsqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        Id = (int)reader["movie_id"];
+                        Title = (string)reader["title"];
+                        Year = (int)reader["year"];
+                        Price = Convert.ToDouble(reader["price"]);
+                    }
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return $"Movie {Id}: {Title} produced in {Year} costs {Price}";
         }
 
         public void Save()
         {
-
+            using(var conn = new NpgsqlConnection(connection_string))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand("UPDATE movies " +
+                    "SET title = @title, year = @year, price = @price " +
+                    "WHERE movie_id = @id", conn))
+                {
+                    cmd.Parameters.AddWithValue("@id", Id);
+                    cmd.Parameters.AddWithValue("@title", Title);
+                    cmd.Parameters.AddWithValue("@year", Year);
+                    cmd.Parameters.AddWithValue("@price", Price);
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
 
         public void Remove()
--- ActiveRecord/ActiveRecord/Program.cs
+++ ActiveRecord/ActiveRecord/Program.cs
@@ -6,7 +6,17 @@ namespace ActiveRecord
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome do DVD rental store");
+            Console.Write("Please provide an ID of a movie: ");
+            int movie_id = int.Parse(Console.ReadLine());
+            MovieRecord movie = new MovieRecord(movie_id);
+            Console.WriteLine(movie.ToString());
+
+            Console.Write("Provice new price for the movie: ");
+            double new_price = double.Parse(Console.ReadLine());
+            movie.Price = new_price;
+            movie.Save();
+
         }
     }
 }
