7f39983733de4ca0a63e0c13d1dbcf3894dab0a1
Szymon Murawski <szymon.murawski@gmail.com>
2021-03-16 14:07:08 +0100

    Skeleton

---
+++ ActiveRecord/ActiveRecord/MovieRecord.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Npgsql;
+
+namespace ActiveRecord
+{
+    class MovieRecord
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public double Price { get; set; }
+        public MovieRecord(int id, string title, int year, double price)
+        {
+            Id = id;
+            Title = title;
+            Year = year;
+            Price = price;
+        }
+
+        public static MovieRecord GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+
+        }
+
+        public void Remove()
+        {
+
+        }
+    }
+}
