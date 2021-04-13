227d36359bcff11ec4ff7e57e03e8676d9f8ce23
Szymon Murawski <szymon.murawski@gmail.com>
2021-04-13 14:04:24 +0200

    Init

---
+++ DVDRentalStoreWebApp/DVDRentalStoreWebApp/Controllers/MoviesController.cs
@@ -0,0 +1,96 @@
+﻿using DVDRentalStoreWebApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DVDRentalStoreWebApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private static List<Movie> Movies = new List<Movie>
+        {
+            new Movie(1, "Anchorman", 2000, 12),
+            new Movie(2, "Anchorman 2", 2001, 7),
+            new Movie(3, "Terminator", 1993, 22),
+            new Movie(4, "Jurrasic Park", 1999, 29),
+            new Movie(5, "The Lord of the Rings", 1997, 82),
+        };
+        // GET: MoviesController
+        public ActionResult Index()
+        {
+            return View(Movies);
+        }
+
+        // GET: MoviesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MoviesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MoviesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MoviesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MoviesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MoviesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MoviesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DVDRentalStoreWebApp/DVDRentalStoreWebApp/Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DVDRentalStoreWebApp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public double Price { get; set; }
+
+        public Movie(int id, string title, int year, double price)
+        {
+            Id = id;
+            Title = title;
+            Year = year;
+            Price = price;
+        }
+        public Movie()
+        {
+
+        }
+    }
+}
