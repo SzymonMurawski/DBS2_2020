a27820baa3909df350445552118945ae0f5d492a
Szymon Murawski <szymon.murawski@gmail.com>
2021-05-11 14:33:27 +0200

    Annotations

--- DVDRentalStoreWebApp/DVDRentalStoreWebApp/Models/Person.cs
+++ DVDRentalStoreWebApp/DVDRentalStoreWebApp/Models/Person.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +10,11 @@ namespace DVDRentalStoreWebApp.Models
     public class Person
     {
         public int Id { get; set; }
+        [Required, MaxLength(50)]
         public string FirstName { get; set; }
+        [Required, MaxLength(50)]
         public string LastName { get; set; }
+        [NotMapped]
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
     }
 }
